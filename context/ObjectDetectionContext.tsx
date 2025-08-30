68586b3bd4bd7a2557d9145135fc72d233e1e9ca
yihao <yihao039@gmail.com>
2025-08-31 04:47:55 +0800

    update

--- context/ObjectDetectionContext.tsx
+++ context/ObjectDetectionContext.tsx
@@ -4,18 +4,21 @@ import { Detection } from "react-native-executorch";
 type ObjectDetectionContextType = {
   detected: Detection[] | null;
   setDetected: (detected: Detection[] | null) => void;
-  textResponse: Detection[] | null;
-  setTextResponse: (detected: Detection[] | null) => void;
+  ocrDetected: String | null;
+  setOcrDetected: (detected: String | null) => void;
+  textResponse: String | null;
+  setTextResponse: (detected: String | null) => void;
 };
 
 const ObjectDetectionContext = createContext<ObjectDetectionContextType | undefined>(undefined);
 
 export const ObjectDetectionProvider = ({ children }: { children: ReactNode }) => {
   const [detected, setDetected] = useState<Detection[] | null>(null);
-  const [textResponse, setTextResponse] = useState<Detection[] | null>(null);
+  const [ocrDetected, setOcrDetected] = useState<String | null>(null);
+  const [textResponse, setTextResponse] = useState<String | null>(null);
 
   return (
-    <ObjectDetectionContext.Provider value={{ detected: detected, setDetected: setDetected, textResponse: textResponse, setTextResponse: setTextResponse }}>
+    <ObjectDetectionContext.Provider value={{ detected, setDetected, ocrDetected, setOcrDetected, textResponse, setTextResponse }}>
       {children}
     </ObjectDetectionContext.Provider>
   );
