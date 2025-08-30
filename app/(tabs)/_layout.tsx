73f4cc95f5bd6d5273806612771d614c31f5f018
yihao <yihao039@gmail.com>
2025-08-30 23:17:58 +0800

    add object detection

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,24 +2,27 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-import {Stack} from 'expo-router';
+import { Stack } from 'expo-router';
 import { ImageProvider } from '@/context/ImageContext';
+import { ObjectDetectionProvider } from '@/context/ObjectDetectionContext';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ImageProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{headerShown: false}} />
-        <Stack.Screen name="imageUpload" options={{headerShown: false}} />
-        <Stack.Screen name="imageScanResult" options={{headerShown: false}} />
-        <Stack.Screen name="imageFiltered" options={{headerShown: false}} />
-        <Stack.Screen name="textUpload" options={{headerShown: false}} />
-        <Stack.Screen name="textScanResult" options={{headerShown: false}} />
-        <Stack.Screen name="textFiltered" options={{headerShown: false}} />
-    </Stack>
-    </ImageProvider>
-    
+    <ObjectDetectionProvider>
+      <ImageProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="imageUpload" options={{ headerShown: false }} />
+          <Stack.Screen name="imageScanResult" options={{ headerShown: false }} />
+          <Stack.Screen name="imageFiltered" options={{ headerShown: false }} />
+          <Stack.Screen name="textUpload" options={{ headerShown: false }} />
+          <Stack.Screen name="textScanResult" options={{ headerShown: false }} />
+          <Stack.Screen name="textFiltered" options={{ headerShown: false }} />
+        </Stack>
+      </ImageProvider>
+    </ObjectDetectionProvider>
+
   );
 }
--- app/(tabs)/imageUpload.tsx
+++ app/(tabs)/imageUpload.tsx
@@ -1,17 +1,13 @@
 import { Image } from 'expo-image';
-import { Platform, Pressable, StyleSheet } from 'react-native';
-import {useState, useEffect} from "react";
+import { Pressable, StyleSheet } from 'react-native';
+import { useState, useEffect } from "react";
 
-import { HelloWave } from '@/components/template/HelloWave';
-import ParallaxScrollView from '@/components/template/ParallaxScrollView';
 import ThemedText from '@/components/ui/ThemedText';
-import { ThemedView } from '@/components/template/ThemedView';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import { Colors } from '@/constants/Colors';
 
-import {Divider} from "react-native-paper"
 import ButtonLight from '@/components/buttons/ButtonLight';
 import TextButton from '@/components/buttons/TextButton';
 
@@ -22,14 +18,17 @@ import * as ImagePicker from 'expo-image-picker';
 import ResponsiveImage from '@/components/ResponsiveImage';
 
 import { useImage } from "../../context/ImageContext"
+import runObjectDetection from './ExecutorchTest';
+import { Detection } from 'react-native-executorch';
 
 export default function ImageUploadScreen() {
   const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const [image, setImage] = useState<string | null>(null);
+  const [result, setResult] = useState<Detection[]>([]);
 
-  const { setImageUri } = useImage();
+  const { setImageUri, imageUri } = useImage();
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -43,31 +42,32 @@ export default function ImageUploadScreen() {
     if (!result.canceled) {
       setImage(result.assets[0].uri);
       setImageUri(result.assets[0].uri);
+      await runObjectDetection((result.assets[0].uri) as string).then(detections => setResult(detections));
     }
   };
 
-  const proceed = async() => {
-    if (image && !loading) {
+  const proceed = async () => {
+    if (imageUri && !loading) {
       setLoading(true);
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      await runObjectDetection(imageUri);
       router.push("./imageScanResult");
     }
   }
 
   // clear off images from prev sessions
-  useEffect(()=> {
+  useEffect(() => {
     setImageUri("");
   }, [])
 
 
   return (
     <>
-    {loading ? 
-    <View style={{zIndex: 5, backgroundColor: "#f2f2f2", ...StyleSheet.absoluteFillObject}}>
-      <LoadingScreen></LoadingScreen>
-    </View> : <></>}
+      {loading ?
+        <View style={{ zIndex: 5, backgroundColor: "#f2f2f2", ...StyleSheet.absoluteFillObject }}>
+          <LoadingScreen></LoadingScreen>
+        </View> : <></>}
 
-    <SafeAreaView>
+      <SafeAreaView>
         <View style={styles.container}>
           <ThemedText fontSize={32} font="Montserrat" weight="Bold" color={Colors.colorPrimary}>
             Scan Your Image
@@ -75,42 +75,47 @@ export default function ImageUploadScreen() {
           <ThemedText fontSize={18} weight="Light">
             Detect faces, documents and private details in your photos
           </ThemedText>
-          
+
           {image ?
-           <ResponsiveImage source={{uri: image}} maxHeight={300} style={{alignSelf: "center", marginTop: 32}}/> :
-           <View style={styles.uploadContainer}>
-            <Pressable onPress={pickImage} style={{
-              alignItems: "center"}}>
-              <Image source={require("../../assets/images/pictureIcon.png")} style={{width: 30, height: 30}}></Image>
-              <ThemedText weight="Light" style={{marginTop: 8}}>Select file</ThemedText>
-            </Pressable>
-          </View>
-           }
-          
+            <ResponsiveImage source={{ uri: image }} maxHeight={300} style={{ alignSelf: "center", marginTop: 32 }} /> :
+            <View style={styles.uploadContainer}>
+              <Pressable onPress={pickImage} style={{
+                alignItems: "center"
+              }}>
+                <Image source={require("../../assets/images/pictureIcon.png")} style={{ width: 30, height: 30 }}></Image>
+                <ThemedText weight="Light" style={{ marginTop: 8 }}>Select file</ThemedText>
+              </Pressable>
+            </View>
+          }
+
           {!image && <View style={styles.divider}>
             <ThemedText style={styles.dividerText}>or</ThemedText>
           </View>}
 
-          {image && <ButtonLight onPress={pickImage} style={{marginTop: 32, width: "100%"}}>
+          {image && <ButtonLight onPress={pickImage} style={{ marginTop: 32, width: "100%" }}>
             <ThemedText fontSize={16} weight="SemiBold" color="#001847">
               Choose another photo</ThemedText>
           </ButtonLight>}
-          
-          <ButtonLight onPress={()=>{}} style={{marginTop: 32, width: "100%"}}>
+
+          <ButtonLight onPress={() => { }} style={{ marginTop: 32, width: "100%" }}>
             <ThemedText fontSize={16} weight="SemiBold" color="#001847">
               Open Camera and Take Photo</ThemedText>
           </ButtonLight>
 
-          <TextButton onPress={proceed} style={{marginTop: 100, alignSelf: "center", paddingVertical: 12, paddingHorizontal: 24}}>
+          <TextButton onPress={proceed} style={{ marginTop: 100, alignSelf: "center", paddingVertical: 12, paddingHorizontal: 24 }}>
             <ThemedText color="#FFF" fontSize={18}>
               Proceed to Scan
             </ThemedText>
+            <ThemedText>
+              {result.map(item => `${item.label} (${(item.score * 100).toFixed(1)}%)`).join(", ")}
+            </ThemedText>
           </TextButton>
-          
+
+
         </View>
-    </SafeAreaView>
+      </SafeAreaView>
     </>
-    
+
   );
 }
 
@@ -135,13 +140,13 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: Colors.colorText,
     marginTop: 32
-    
+
   },
   dividerText: {
     position: "absolute",
     left: "50%",
     bottom: 0,
-    transform: [{translateX: "-50%"}, {translateY: "50%"}],
+    transform: [{ translateX: "-50%" }, { translateY: "50%" }],
     backgroundColor: "#f2f2f2",
     paddingHorizontal: 16
   }
