6a5e16acc01c10f39ba04cba548aa29a79eea33e
JoanneHing <yenxinghing@gmail.com>
2025-08-30 22:33:58 +0800

    Update loading pages

--- app/(tabs)/imageUpload.tsx
+++ app/(tabs)/imageUpload.tsx
@@ -1,5 +1,6 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, Pressable, StyleSheet } from 'react-native';
+import {useState, useEffect} from "react";
 
 import { HelloWave } from '@/components/template/HelloWave';
 import ParallaxScrollView from '@/components/template/ParallaxScrollView';
@@ -10,9 +11,62 @@ import { Text, View } from 'react-native';
 
 import { Colors } from '@/constants/Colors';
 
+import {Divider} from "react-native-paper"
+import ButtonLight from '@/components/buttons/ButtonLight';
+import TextButton from '@/components/buttons/TextButton';
+
+import LoadingScreen from '@/components/LoadingScreen';
+
+import { useRouter } from 'expo-router';
+import * as ImagePicker from 'expo-image-picker';
+import ResponsiveImage from '@/components/ResponsiveImage';
+
+import { useImage } from "../../context/ImageContext"
 
 export default function ImageUploadScreen() {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const [image, setImage] = useState<string | null>(null);
+
+  const { setImageUri } = useImage();
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images', 'videos'],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      setImageUri(result.assets[0].uri);
+    }
+  };
+
+  const proceed = async() => {
+    if (image && !loading) {
+      setLoading(true);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      router.push("./imageScanResult");
+    }
+  }
+
+  // clear off images from prev sessions
+  useEffect(()=> {
+    setImageUri("");
+  }, [])
+
+
   return (
+    <>
+    {loading ? 
+    <View style={{zIndex: 5, backgroundColor: "#f2f2f2", ...StyleSheet.absoluteFillObject}}>
+      <LoadingScreen></LoadingScreen>
+    </View> : <></>}
+
     <SafeAreaView>
         <View style={styles.container}>
           <ThemedText fontSize={32} font="Montserrat" weight="Bold" color={Colors.colorPrimary}>
@@ -21,19 +75,49 @@ export default function ImageUploadScreen() {
           <ThemedText fontSize={18} weight="Light">
             Detect faces, documents and private details in your photos
           </ThemedText>
-          <View style={styles.uploadContainer}>
-            <Image source={require("../../assets/images/pictureIcon.png")} style={{width: 30, height: 30}}></Image>
-
+          
+          {image ?
+           <ResponsiveImage source={{uri: image}} maxHeight={300} style={{alignSelf: "center", marginTop: 32}}/> :
+           <View style={styles.uploadContainer}>
+            <Pressable onPress={pickImage} style={{
+              alignItems: "center"}}>
+              <Image source={require("../../assets/images/pictureIcon.png")} style={{width: 30, height: 30}}></Image>
+              <ThemedText weight="Light" style={{marginTop: 8}}>Select file</ThemedText>
+            </Pressable>
           </View>
+           }
+          
+          {!image && <View style={styles.divider}>
+            <ThemedText style={styles.dividerText}>or</ThemedText>
+          </View>}
+
+          {image && <ButtonLight onPress={pickImage} style={{marginTop: 32, width: "100%"}}>
+            <ThemedText fontSize={16} weight="SemiBold" color="#001847">
+              Choose another photo</ThemedText>
+          </ButtonLight>}
+          
+          <ButtonLight onPress={()=>{}} style={{marginTop: 32, width: "100%"}}>
+            <ThemedText fontSize={16} weight="SemiBold" color="#001847">
+              Open Camera and Take Photo</ThemedText>
+          </ButtonLight>
+
+          <TextButton onPress={proceed} style={{marginTop: 100, alignSelf: "center", paddingVertical: 12, paddingHorizontal: 24}}>
+            <ThemedText color="#FFF" fontSize={18}>
+              Proceed to Scan
+            </ThemedText>
+          </TextButton>
+          
         </View>
     </SafeAreaView>
+    </>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 40,
-    paddingHorizontal: 20
+    paddingTop: 70,
+    paddingHorizontal: 24
   },
   uploadContainer: {
     borderRadius: 32,
@@ -45,5 +129,20 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "column"
+  },
+  divider: {
+    width: "100%",
+    borderBottomWidth: 1,
+    borderColor: Colors.colorText,
+    marginTop: 32
+    
+  },
+  dividerText: {
+    position: "absolute",
+    left: "50%",
+    bottom: 0,
+    transform: [{translateX: "-50%"}, {translateY: "50%"}],
+    backgroundColor: "#f2f2f2",
+    paddingHorizontal: 16
   }
 });
--- app/(tabs)/textUpload.tsx
+++ app/(tabs)/textUpload.tsx
@@ -7,11 +7,13 @@ import { View, TextInput } from 'react-native';
 import { useRouter } from 'expo-router';
 import { useState } from 'react';
 import * as Clipboard from 'expo-clipboard';
+import LoadingScreen from '@/components/LoadingScreen';
 
 import { Colors } from '@/constants/Colors';
 import TextButton from '@/components/buttons/TextButton';
 
 export default function TextUploadScreen() {
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const [textInput, setTextInput] = useState('');
 
@@ -29,51 +31,62 @@ export default function TextUploadScreen() {
   };
 
   // If not text input, do not allow to proceed
-  const handleProceedToScan = () => {
+  const handleProceedToScan = async() => {
     if (textInput.trim() === '') {
       Alert.alert('Please enter text to scan');
       return;
     }
-    router.push('./textScanResult');
+
+    if (!isLoading) {
+      setIsLoading(true);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      router.push('./textScanResult');
+    }
   };
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.container}>
-        <ThemedText fontSize={32} font="Montserrat" weight="Bold" color={Colors.colorPrimary}>
-          Scan Your Text
-        </ThemedText>
-        <ThemedText fontSize={18} weight="Light" style={styles.subtitle}>
-          Check your captions and text for any sensitive or private information.
-        </ThemedText>
-        
-        <TextButton style={styles.pasteButton} onPress={handlePasteFromClipboard}>
-          <ThemedText fontSize={16} weight="Bold" color="white">
-            Paste from Clipboard
+    <>
+      {isLoading ? 
+        <View style={{zIndex: 5, backgroundColor: "#f2f2f2", ...StyleSheet.absoluteFillObject}}>
+          <LoadingScreen></LoadingScreen>
+        </View> : <></>}
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <ThemedText fontSize={32} font="Montserrat" weight="Bold" color={Colors.colorPrimary}>
+            Scan Your Text
           </ThemedText>
-        </TextButton>
+          <ThemedText fontSize={18} weight="Light" style={styles.subtitle}>
+            Check your captions and text for any sensitive or private information.
+          </ThemedText>
+          
+          <TextButton style={styles.pasteButton} onPress={handlePasteFromClipboard}>
+            <ThemedText fontSize={16} weight="Bold" color="white">
+              Paste from Clipboard
+            </ThemedText>
+          </TextButton>
 
-        {/* Text Input Container */}
-        <View style={styles.textInputContainer}>
-          <TextInput
-            style={styles.textInput}
-            multiline={true}
-            textAlignVertical="top"
-            placeholder="Input your text or caption here..."
-            placeholderTextColor="#999"
-            value={textInput}
-            onChangeText={setTextInput}
-          />
+          {/* Text Input Container */}
+          <View style={styles.textInputContainer}>
+            <TextInput
+              style={styles.textInput}
+              multiline={true}
+              textAlignVertical="top"
+              placeholder="Input your text or caption here..."
+              placeholderTextColor="#999"
+              value={textInput}
+              onChangeText={setTextInput}
+            />
+          </View>
+
+          {/* Proceed to Scan Button */}
+          <TextButton style={styles.scanButton} onPress={handleProceedToScan}>
+            <ThemedText fontSize={18} weight="Bold" color="white">
+              Proceed to Scan
+            </ThemedText>
+          </TextButton>
         </View>
-
-        {/* Proceed to Scan Button */}
-        <TextButton style={styles.scanButton} onPress={handleProceedToScan}>
-          <ThemedText fontSize={18} weight="Bold" color="white">
-            Proceed to Scan
-          </ThemedText>
-        </TextButton>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </>
   );
 }
 
